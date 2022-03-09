905da53fd6b28d7215cbc7aa5bd009c7c19da75b
anggiie <anggiie02>
2022-03-09 21:56:32 +0100

    add AuthModal

---
+++ src/components/AuthModal.js
@@ -0,0 +1,14 @@
+
+const AuthModal = ({setShowModal}) => {
+
+    const handleClick = () => {
+        setShowModal(false)
+    }
+
+    return (
+        <div className="auth-modal">
+            <div onClick={handleClick}>⨂</div>
+        </div>
+    )
+}
+export default AuthModal

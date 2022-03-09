591b12434d14ac9fd37620ee762aa0f8c2e0da38
anggiie <anggiie02>
2022-03-09 21:58:21 +0100

    add nav and modifs homepage

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,16 @@
 import whiteLogo from '../images/tiinder-white.png'
 import colorLogo from '../images/tinder-rond.png'
 
-const Nav = ({ minimal }) => {
+const Nav = ({ minimal, authToken }) => {
 
     return (
         <nav>
             <div className="logo-container">
                 <img src={minimal ? colorLogo : whiteLogo} alt="" className="logo" />
             </div>
+
+            {!authToken && !minimal && <button 
+            className="nav-button">Log in </button>}
         </nav>
     )
 }

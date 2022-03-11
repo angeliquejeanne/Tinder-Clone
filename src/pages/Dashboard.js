489b10fb0120eddee45393bb47e3191209264fc3
anggiie <anggiie02>
2022-03-11 08:36:24 +0100

    end OnBoarding page, create Dashboard page and start ChatContainer

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,6 +1,65 @@
+import TinderCard from "react-tinder-card"
+import { useState } from "react"
+import ChatContainer from '../components/ChatContainer'
+
 const Dashboard = () => {
+    const characters = [
+        {
+            name: 'Richard Hendricks',
+            url: 'https://images.unsplash.com/photo-1453396450673-3fe83d2db2c4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
+        },
+        {
+            name: 'Erlich Bachman',
+            url: 'https://images.unsplash.com/photo-1453396450673-3fe83d2db2c4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
+        },
+        {
+            name: 'Monica Hall',
+            url: 'https://images.unsplash.com/photo-1453396450673-3fe83d2db2c4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
+        },
+        {
+            name: 'Jared Dunn',
+            url: 'https://images.unsplash.com/photo-1453396450673-3fe83d2db2c4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
+        },
+        {
+            name: 'Dinesh Chugtai',
+            url: 'https://images.unsplash.com/photo-1453396450673-3fe83d2db2c4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
+        }
+    ]
+
+    const [lastDirection, setLastDirection] = useState()
+
+    const swiped = (direction, nameToDelete) => {
+        console.log('removing:' + nameToDelete)
+        setLastDirection(direction)
+    }
+
+    const outOfFrame = (name) => {
+        console.log(name + ' left the screen!')
+    }
+
     return (
-        <div>Dashboard</div>
+        <div className="dashboard">
+            <ChatContainer />
+            <div className="swiper-container">
+                <div className="card-container">
+                    {characters.map((character) =>
+                        <TinderCard 
+                            className="swipe" 
+                            key={character.name} 
+                            onSwipe={(dir) => swiped(dir, character.name)}
+                            onCardLeftScreen={() => outOfFrame(character.name)}
+                        >
+                            <div className="card" style={{ backgroundImage: 'url(' + character.url + ')'}}>
+                                <h3>{character.name}</h3>
+                            </div>
+                        </TinderCard>
+                    )}
+                    <div className="swipe-info">
+                        {lastDirection ? <p>You swiped {lastDirection}</p> : <p/>}
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 export default Dashboard

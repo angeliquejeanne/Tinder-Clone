a8e67ce823e9fe05e442de50f509c59fddcf865a
anggiie <anggiie02>
2022-03-12 23:44:32 +0100

    add correction AuthModal, add server folder & add user

--- client/src/components/AuthModal.js
+++ client/src/components/AuthModal.js
@@ -1,10 +1,16 @@
 import { useState } from 'react'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+import { useCookies } from "react-cookie";
 
 const AuthModal = ({ setShowModal, isSignUp }) => {
     const [email, setEmail] = useState(null)
     const [password, setPassword] = useState(null)
     const [confirmPassword, setConfirmPassword] = useState(null)
     const [error, setError] = useState(null)
+    const [cookies, setCookie, removeCookie] = useCookies(["user"])
+
+    let navigate = useNavigate()
 
     console.log(email, password, confirmPassword)
 
@@ -13,18 +19,36 @@ const AuthModal = ({ setShowModal, isSignUp }) => {
         setShowModal(false)
     }
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault()
         try {
-            if( isSignUp && ( password === confirmPassword)) {
-                setError('Passwords need to match!')
-            } 
-            console.log('make a post request to our database')
-        } catch (error) {
-            console.log(error)
+          if (isSignUp && password !== confirmPassword) {
+            setError("Passwords need to match!")
+            return
+          }
+          // console.log("Make a post request to our database");
+          const response = await axios.post(
+            `http://localhost:8000/${isSignUp ? "signup" : "login"}`,
+            {
+              email,
+              password,
+            }
+          );
+    
+          setCookie("AuthToken", response.data.token)
+          setCookie("UserId", response.data.userId)
+    
+          const success = response.status === 201;
+    
+          if (success && isSignUp) navigate("/onboarding")
+          if (success && !isSignUp) navigate("/dashboard")
+    
+          window.location.reload()
+        } catch (err) {
+          console.log(err)
         }
-    }
-
+      }
+    
     return (
         <div className="auth-modal">
             <div className="close-icon" onClick={handleClick}>⨂</div>
